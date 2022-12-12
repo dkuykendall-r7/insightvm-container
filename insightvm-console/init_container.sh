b591094037c51c881f7ce86268ee29b605380f02
dkuykendall-r7 <dan_kuykendall@rapid7.com>
2022-12-12 09:48:41 -0800

    Updated to create smaller 3GB container sizes and use less volume mounts
    All the data except the database is now in a single volume. Keeps things much cleaner.

---
+++ insightvm-console/init_container.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+DATADIR="/opt/rapid7/persistent_data"
+echo "CONTAINER NOTICE: Checking if this is a first time launch"
+if [ "$(ls -A $DATADIR)" ]; then
+    echo "CONTAINER NOTICE: Data in place, continue launching..."
+else
+    echo "CONTAINER NOTICE: Looks like the first run, will move default data files into place."
+    mv /opt/rapid7/default_data/nsc-nxpgsql/* /opt/rapid7/nexpose/nsc/nxpgsql/
+    rmdir /opt/rapid7/default_data/nsc-nxpgsql
+    mv /opt/rapid7/default_data/* /opt/rapid7/persistent_data/
+    echo "CONTAINER NOTICE: Data is now in place, continue launching..."
+fi
+echo "CONTAINER NOTICE: Cleaning up default data"
+rm -rf /opt/rapid7/default_data/
+
+echo "CONTAINER NOTICE: Link persistent data paths"
+cd /opt/rapid7
+ln -s /opt/rapid7/persistent_data/nse-conf      /opt/rapid7/nexpose/nse/conf
+ln -s /opt/rapid7/persistent_data/nse-keystores /opt/rapid7/nexpose/nse/keystores
+
+ln -s /opt/rapid7/persistent_data/nsc-conf      /opt/rapid7/nexpose/nsc/conf
+ln -s /opt/rapid7/persistent_data/nsc-keystores /opt/rapid7/nexpose/nsc/keystores
+ln -s /opt/rapid7/persistent_data/nsc-logs      /opt/rapid7/nexpose/nsc/logs
+ln -s /opt/rapid7/persistent_data/nsc-reports   /opt/rapid7/nexpose/nsc/htroot/reports
+#ln -s /opt/rapid7/persistent_data/nsc-nxpdata   /opt/rapid7/nexpose/nsc/nxpgsql/nxpdata
+echo "CONTAINER NOTICE: Ready to launch Nexpose Console (./nsc.sh)"
+cd /opt/rapid7/nexpose/nsc
+./nsc.sh
+tail -f /dev/null
---
+++ insightvm-scanengine/init_container.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+DATADIR="/opt/rapid7/persistent_data"
+echo "CONTAINER NOTICE: Checking if this is a first time launch"
+if [ "$(ls -A $DATADIR)" ]; then
+    echo "CONTAINER NOTICE: Data in place, continue launching..."
+else
+    echo "CONTAINER NOTICE: Looks like the first run, will move default data files into place."
+    mv /opt/rapid7/default_data/* /opt/rapid7/persistent_data/
+    echo "CONTAINER NOTICE: Data is now in place, continue launching..."
+
+    
+fi
+echo "CONTAINER NOTICE: Cleaning up default data"
+rm -rf /opt/rapid7/default_data/
+
+echo "CONTAINER NOTICE: Link persistent data paths"
+cd /opt/rapid7
+ln persistent_data/nse-conf       nexpose/nse/conf
+ln persistent_data/nse-keystores  nexpose/nse/keystores
+
+
+
+
+
+
+echo "CONTAINER NOTICE: Ready to launch Nexpose Console (./nsc.sh)"
+cd /opt/rapid7/nexpose/nse
+./nse.sh
+tail -f /dev/null
+#script /dev/null
+#screen ./nse.sh
